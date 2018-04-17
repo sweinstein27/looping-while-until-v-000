d9721f716e3744650e0b156e9fe6d93a5fae8bd1
Scott Weinstein <sweinstein27@gmail.com>
2018-04-17 17:40:31 +0000

    automatically backed up by learn

--- lib/while.rb
+++ lib/while.rb
@@ -1,13 +1,13 @@
-def using_while
-	levitation_force = 1
-counter = 0
-loop do
-	while levitation_force == 10
-	puts "Wingardium Leviosa"
-	levitation_force += 1
-	counter +=
-	if counter == 4
-		break
-	end
-end
-end
+# def using_while
+# 	levitation_force = 1
+# counter = 0
+# loop do
+# 	while levitation_force == 10
+# 	puts "Wingardium Leviosa"
+# 	levitation_force += 1
+# 	counter +=
+# 	if counter == 4
+# 		break
+# 	end
+# end
+# end
